b23511604869a4fd2d04928aace1c1dbf1e2d71d
Tomasz Klekner <tomasz@gpmd.co.uk>
2020-03-25 13:24:06 +0100

    Add note submition and render in notes list

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ class App {
 		this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
 		this.$formButtons = document.querySelector("#form-buttons");
+    this.$placeholder = document.querySelector("#placeholder");
+    this.$notes = document.querySelector("#notes");
 
 		this.addEventListeners();
 	}
@@ -14,7 +16,7 @@ class App {
 		document.body.addEventListener("click", event => {
 			this.haldleFormClick(event);
 		});
-    
+
     this.$form.addEventListener('submit', event => {
       event.preventDefault();
 
@@ -48,6 +50,8 @@ class App {
 		this.$form.classList.remove("form-open");
 		this.$noteTitle.style.display = "none";
 		this.$formButtons.style.display = "none";
+    this.$noteTitle.value = '';
+    this.$noteText.value = '';
 	}
   addNote(note) {
     const newNote = {
@@ -57,7 +61,28 @@ class App {
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
     };
     this.notes = [...this.notes, newNote];
-    console.log(this.notes)
+    
+    this.displaynotes();
+    this.closeForm();
+  }
+
+  displaynotes() {
+    const hasNotes = this.notes.length > 0;
+
+    this.$placeholder.style = hasNotes ? 'none' : 'flex';
+
+    this.$notes.innerHTML = this.notes.map(note => `
+      <div style="background: ${note.color};" class="note">
+        <div class="${note.title && 'note-title'}">${note.title}</div>
+        <div class="${note.text && 'note-text'}">${note.text}</div>
+        <div class="toolbar-container">
+          <div class="toolbar">
+            <img class="toolbar-color" src="https://icon.now.sh/palette">
+            <img class="toolbar-delete" src="https://icon.now.sh/delete">
+          </div>
+        </div>
+      </div>
+    `).join("");
   }
 }
 
